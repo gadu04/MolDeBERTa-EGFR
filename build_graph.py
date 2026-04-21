f8f7c614adfa3504ddcecdc2bafba3334616f7f5
seina <seinarugaru@gmail.com>
2026-04-22 01:33:03 +0700

    Update README

--- build_graph.py
+++
@@ -1,3 +0,0 @@
-"""Compatibility wrapper for legacy imports."""
-
-from kg.build_graph import *  # noqa: F401,F403
--- kg_encoder.py
+++
@@ -1,3 +0,0 @@
-"""Compatibility wrapper for legacy imports."""
-
-from kg.kg_encoder import *  # noqa: F401,F403
